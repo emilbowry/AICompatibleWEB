a9d39edd4f9559815057add19cee3ff7b5840b26
emilbowry <emil.bowry@icloud.com>
2025-11-05 15:41:40 +0000

    aa

--- server/src/routes/data.ts
+++ server/src/routes/data.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
-import { getJourneyData } from "../controllers/data/journeydata.js";
 import { getTimelineData } from "../controllers/data/timeline_data.js";
-//
+
 const router = Router();
-router.get("/journey_data", getJourneyData);
 router.get("/timeline", getTimelineData);
 
 export default router;
