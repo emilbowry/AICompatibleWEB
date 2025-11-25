8322ab3c9a8fce6f8220cc3342a932c43f548824
emilbowry <emil.bowry@icloud.com>
2025-11-25 18:13:20 +0000

    ran build

--- server/dist/ssr/adminrenderer.js
+++
@@ -1,3 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-const AdminPortal = (_jsx("div", { style: { height: "500px", width: "500px", background: "red" }, children: _jsx("h1", { style: { color: "black" }, children: "Hello Testing" }) }));
-export { AdminPortal };
--- server/src/ssr/adminrenderer.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const AdminPortal: React.ReactNode = (
-	<div style={{ height: "500px", width: "500px", background: "red" }}>
-		<h1 style={{ color: "black" }}>Hello Testing</h1>
-	</div>
-);
-export { AdminPortal };
